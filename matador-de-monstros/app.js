45e9b70f2a697e9d6872e225d512126ce428a8ac
Bernardo Simonassi <bernardosimonassi@hotmail.com>
2019-11-14 19:28:05 -0300

    atualizando o projeto do matador de monstros

--- matador-de-monstros/app.js
+++ matador-de-monstros/app.js
@@ -7,31 +7,42 @@ new Vue({
 		curaJogador: Math.floor(Math.random() * 6 + 7),
 		vidaMonstro: 100,
 		ataqueMonstro: Math.floor(Math.random() * 4 + 8),
-		relatorio: []
+		logs: []
 	},
 	methods: {
-		iniciarEfeito() {
-			setInterval(() => {
-				this.efeitoCSS = this.efeitoCSS == 'destaque' 
-					? 'encolher' : 'destaque'
-			}, 1000)
+		ataque() {
+			this.ataqueJogador = Math.floor(Math.random() * 6 + 6);
+			this.ataqueMonstro = Math.floor(Math.random() * 4 + 8);
+			this.vidaMonstro = this.vidaMonstro - this.ataqueJogador;
+			this.vidaJogador = this.vidaJogador - this.ataqueMonstro;
+			this.registerLog('Ataque jogador:'+this.ataqueJogador, 'Ataque monstro:'+this.ataqueMonstro);
 		},
-		iniciarProgresso() {
-			setInterval(() => {
-				this.largura++;
-				this.progress = this.largura + '%';
-
-				if(this.largura == 100){
-					this.largura = 0;
-				}
-			}, 50)
+		ataqueEspecial() {
+			this.ataqueEspJogador = Math.floor(Math.random() * 6 + 8);
+			this.ataqueMonstro = Math.floor(Math.random() * 4 + 8);
+			this.vidaMonstro = this.vidaMonstro - this.ataqueEspJogador;
+			this.vidaJogador = this.vidaJogador - this.ataqueMonstro;
+			this.registerLog('Ataque especial jogador:'+this.ataqueEspJogador, 'Ataque monstro:'+this.ataqueMonstro);
 		},
-		ativarPerigo(event) {
-			if(event.target.value == 'true'){
-				this.perigoAlterar = 'perigo';
-			}else if(event.target.value == 'false'){
-				this.perigoAlterar = '';
-			} 
+		cura() {
+			this.curaJogador = Math.floor(Math.random() * 6 + 7);
+			this.ataqueMonstro = Math.floor(Math.random() * 4 + 8);
+			this.vidaJogador = this.vidaJogador + this.curaJogador;
+			this.vidaJogador = this.vidaJogador - this.ataqueMonstro;
+			this.registerLog('Cura jogador:'+this.curaJogador, 'Ataque monstro:'+this.ataqueMonstro);
+		},
+		desistir() {
+			this.vidaMonstro = 100;
+			this.vidaJogador = 100;
+			this.logs = [];
+		},
+		registerLog(textJogador, textMonstro) {
+			this.logs.unshift({textJogador, textMonstro});
+		}
+	},
+	computed: {
+		zerarVida() {
+			return this.vidaJogador <= 0 ? this.vidaJogador = 0 : this.vidaJogador
 		}
 	}
 })
